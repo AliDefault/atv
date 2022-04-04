2ddf4284046cd45b41748bf9dd0c93caf121c387
AlissoLevi <alissonlevi24@gmail.com>
2022-04-04 16:33:34 -0300

    1.0.0

---
+++ home.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>SempreAqui - Home</title>
+</head>
+<body>
+    <nav>
+        <ul class="menu">
+            <li><a href="">Home</a></li>
+            <li><a href="">Vagas</a></li>
+            <li><a href="">Sobre</a></li>
+            <li><a href="" id="login-op">Login</a></li>
+        </ul>
+    </nav>
+    <section class="home">
+        <div class="container-img">
+        </div>
+        <div class="container-introducao">
+            <h1>A procura de vaga de <spam id="destaque">emprego?</spam></h1>
+            <p id="p-introducao">Só aqui na SempreAqui você encontra as melhores opções para conseguir sua vaga de emprego.</p>
+        </div>
+    </section>
+</body>
+</html>
